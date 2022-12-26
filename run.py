1ee79820a782ec457277f560a4abf832a5dbd25f
iyrepl <111183314+iyrepl@users.noreply.github.com>
2022-12-26 20:07:56 +0800

    Update run.py

--- run.py
+++ run.py
@@ -4,14 +4,14 @@ url = 'https://panel.ct8.pl/login/'
 username = 'iytest'
 password = 'Tsglockey!1'
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
 }
 data = {
     'user': username,
     'pass': password,
 }
 
-response = requests.post(url, headers=headers, data=data)
+response = requests.post(url, headers = {'user-agent': 'anything'}, data=data)
 if response.status_code == 200:
     print("Successfully logged in as {username}".format(username=username))
 else:
