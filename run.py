d599251d2d78f91a84f62d82721894bae792224f
iyrepl <111183314+iyrepl@users.noreply.github.com>
2022-12-26 20:18:39 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 import pycpanel
 
-server = pycpanel.conn(hostname='panel.ct8.pl', username='iytest', hash=None, password='Tsglockey!1', ssl=True, verify=False, check_conn=False)
+server = pycpanel.conn(hostname='panel.ct8.pl', username='rendorg', hash=None, password='Tsglockey!1', ssl=True, verify=False, check_conn=False)
