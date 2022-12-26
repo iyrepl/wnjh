59aef43e00babd85734c36d6e0b059aef4954ae1
iyrepl <111183314+iyrepl@users.noreply.github.com>
2022-12-26 19:51:42 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,22 +1,18 @@
-version: 2.1
+import requests
 
-jobs:
-  extend:
-    docker:
-      - image: cimg/python:3.11.1
-    resource_class: small
-    steps:
-      - checkout
-      - run: 
-          name: Upgrade pip
-          command: /home/circleci/.pyenv/versions/3.11.1/bin/python3.11 -m pip install --upgrade pip
-      - run: 
-          name: Insall Dependices
-          command: pip3 install requests
-      - run: 
-          name: Extend
-          command: python3 run.py
-workflows:
-  extend:
-    jobs:
-      - extend
+url = 'https://panel.ct8.pl/login/'
+username = 'iytest'
+password = 'Tsglockey!1'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0',
+}
+data = {
+    'user': username,
+    'pass': password,
+}
+
+response = requests.post(url, headers=headers, data=data)
+if response.status_code == 200:
+    print "Successfully logged in as {username}".format(username=username)
+else:
+    print "Login unsuccessful: HTTP/{status_code}".format(status_code=response.status_code
